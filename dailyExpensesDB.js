41268c4c308484cd9ff8c33817b93d1274934e6a
agnobayiS <C.ya@live.com>
2022-09-27 15:19:46 +0200

    first commit

---
+++ dailyExpensesDB.js
@@ -0,0 +1,50 @@
+module.exports = function waiters(db) {
+
+
+    async function validateLogIns(name) {
+
+        let nameRegex = /^[a-zA-Z]{3,}$/
+        let emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+
+
+        const names = nameRegex.test(name);
+        // const surnames = nameRegex.test(surname)
+        // const emails = emailRegex.test(email)
+
+
+        if (names) {
+
+    
+           return await db.none('insert into login (names) values ($1)', [names])
+
+        }
+
+    }
+    async function checkUser(users) {
+
+        let user = await db.any('select name from logins where name = $1', [users])
+        return user.length >= 1 ? true : false;
+    }
+
+    async function insertExpence(date,expence,amount,name_id,catergory_Id){
+
+      return await db.none('insert into tablereff (DATE,EXPENCESENAME,AMOUNT,NAME_ID,CATEGORY_ID) values($1,$2,$3,$4,$5)',[date,expence,amount,name_id,catergory_Id])
+
+
+
+    }
+
+    async function getuser(name){
+
+        let user = await db.manyOrNone('select * from logins where names = $1',[name])
+        return user
+    }
+
+
+    return {
+        validateLogIns,
+        checkUser,
+        insertExpence,
+        getuser
+    }
+}
---
+++ index.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const flash = require('express-flash');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+const session = require('express-session');
+const { render } = require('ejs');
+const dailyFF = require('./dailyExpensesDB')();
+// const routes = require('./routs/waters-routs')
+
+const local_database_url = 'postgres://siyabonga:siya@localhost:5432/daily_expenses';
+const connectionString = process.env.DATABASE_URL || local_database_url;
+
+
+const app = express();
+
+
+const config = {
+    connectionString
+}
+if (process.env.NODE_ENV == "production") {
+    config.ssl = {
+        rejectUnauthorized: false
+    }
+}
+
+// const db = pgp(config);
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static("public"))
+
+
+app.use(session({
+    secret: "<add a secret string here>",
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+app.use(flash());
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+
+app.use(
+    session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+
+
+    })
+);
+
+
+
+
+app.get('/', function (req, res) {
+
+
+
+    res.render("index", {
+
+    })
+})
+
+
+app.post('/adduser', async function (req, res) {
+
+    const name = req.body.name
+
+    if(name){
+        await dailyFF.validateLogIns(name)
+
+    }
+    const getuser = await dailyFF.getuser(name)
+    const getname = getuser.name
+    
+    res.redirect(`/adduser/${getname}`)
+})
+
+app.get('/adduser:', async function(req,res){
+    
+
+    res.render('expence',{
+        
+    })
+
+})
+
+app.get('/expenses',async function (req,res){
+
+    const amount = req.body.amount
+    const type = req.body.btn
+
+    if(amount && type){
+        await dailyFF.insertExpence()
+    }
+
+    res.render('admin',{
+
+    })
+})
+
+app.get('/waiters/:username', function (req, res) {
+
+
+
+})
+
+
+
+
+const PORT = process.env.PORT || 3052;
+
+app.listen(PORT, function () {
+    console.log("App started at port:", PORT)
+});
